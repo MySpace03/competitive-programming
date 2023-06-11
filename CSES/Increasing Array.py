adba1563dc156df7cd9c3ca7489af16f99cd9bfc
MySpace03 <120674982+MySpace03@users.noreply.github.com>
2023-06-11 11:43:33 +0530

    Delete Increasing Array.py

--- CSES/Increasing Array.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-arr = list(map(int,input().split()))
-s = 0
-for i in range(1,n,1):
-    t = arr[i] - arr[i-1]
-    if t<0:
-        s += (-1*t) 
-        arr[i] = arr[i-1]
-print(s)
